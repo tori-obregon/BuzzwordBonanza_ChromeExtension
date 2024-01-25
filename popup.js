9e81ef19d1152af4a84867b7a8174f3fda38dbe8
tori-obregon <vobregon@gmail.com>
2024-01-24 22:33:51 -0800

    finished extension

--- popup.js
+++ popup.js
@@ -3,14 +3,17 @@ const search = document.getElementById('search');
 const word = document.getElementById('wordInput');
 const displayWord = document.getElementById('word');
 const displayDefinition = document.getElementById('definition');
-
+const definitionPage = document.getElementById('definitionPage');
+const frontCover = document.getElementById('wordInputContainer');
+const resetButton = document.getElementById('reset');
 
 const apiKey = "rl4fAPtIQ2wRhdSyKEdGfw==G8gVVONAtSijPD3n"
 const apiUrl = "https://api.api-ninjas.com/v1/dictionary?word="
 
 search.addEventListener('click', () => {
 const wordToSearch = word.value;
-if(wordToSearch !== ''){
+console.log(`${apiUrl}${wordToSearch}`);
+if(wordToSearch){
 fetch(`${apiUrl}${wordToSearch}`,{
     headers: {
         'X-Api-Key': apiKey
@@ -18,15 +21,21 @@ fetch(`${apiUrl}${wordToSearch}`,{
 })
 .then(response => response.json())
 .then(data => {
+    console.log(data)
     displayWord.textContent = wordToSearch;
     displayDefinition.textContent = data.definition;
-})
+    definitionPage.style.display = 'flex';
+    frontCover.style.display = 'none';
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+    }
+});
 
-
-
-
-
-}
-})
-
+resetButton.addEventListener('click', () => {
+    word.value = null;
+    definitionPage.style.display = 'none';
+    frontCover.style.display = 'flex';
+});
 
