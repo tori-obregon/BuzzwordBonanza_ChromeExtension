e8d0cf4381aa0424135ce0a292278c29f3e9b578
tori-obregon <vobregon@gmail.com>
2024-01-24 20:40:58 -0800

    Front and Inside Pages done, api added

--- popup.js
+++ popup.js
@@ -1,1 +1,32 @@
 console.log('This is a popup!');
+const search = document.getElementById('search');
+const word = document.getElementById('wordInput');
+const displayWord = document.getElementById('word');
+const displayDefinition = document.getElementById('definition');
+
+
+const apiKey = "rl4fAPtIQ2wRhdSyKEdGfw==G8gVVONAtSijPD3n"
+const apiUrl = "https://api.api-ninjas.com/v1/dictionary?word="
+
+search.addEventListener('click', () => {
+const wordToSearch = word.value;
+if(wordToSearch !== ''){
+fetch(`${apiUrl}${wordToSearch}`,{
+    headers: {
+        'X-Api-Key': apiKey
+    }
+})
+.then(response => response.json())
+.then(data => {
+    displayWord.textContent = wordToSearch;
+    displayDefinition.textContent = data.definition;
+})
+
+
+
+
+
+}
+})
+
+
